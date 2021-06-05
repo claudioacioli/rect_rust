dfc14dc2d84685117d30d28eb10e3b289abcb544
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-04 23:48:31 -0300

    feat: Add simple way calculate area of rect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let length: u32 = 50;
+    let width: u32 = 30;
+    println!(
+        "The area of rectangle is {} square pixels.",
+        area(&length, &width)
+    );
+}
+
+fn area (length: &u32, width: &u32) -> u32 {
+    length * width
 }
