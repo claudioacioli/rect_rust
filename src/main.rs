eba86a7acd95322159ede9f7e262a7c73b035754
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-05 00:03:31 -0300

    refactor: Use simple struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+struct Rectangle {
+    length: u16,
+    width: u16
+}
+
 fn main() {
-    let rect:(u16, u16) = (50, 30);
+    let rect:Rectangle = build_rect(50, 30);
 
     println!(
         "The area of rectangle is {} square pixels.",
@@ -7,6 +12,13 @@ fn main() {
     );
 }
 
-fn area (rect: &(u16, u16)) -> u16 {
-    rect.0 * rect.1
+fn area (rect:&Rectangle) -> u16 {
+    rect.length * rect.width
+}
+
+fn build_rect(length:u16, width:u16) -> Rectangle {
+    Rectangle {
+        length,
+        width
+    }
 }
