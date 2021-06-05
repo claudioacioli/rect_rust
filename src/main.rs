87f5df3c5b8a9fb3e713785ab6fceda3833e713d
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2021-06-05 00:08:59 -0300

    refactor: Use method sintax

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,19 @@ struct Rectangle {
     width: u16
 }
 
+impl Rectangle {
+    fn area (&self) -> u16 {
+        self.width * self.length
+    }
+}
+
 fn main() {
     let rect:Rectangle = build_rect(50, 30);
 
     println!(
         "The area of rectangle is {} square pixels.",
-        area(&rect)
+        rect.area()
     );
-}
-
-fn area (rect:&Rectangle) -> u16 {
-    rect.length * rect.width
 }
 
 fn build_rect(length:u16, width:u16) -> Rectangle {
